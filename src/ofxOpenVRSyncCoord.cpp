d638df78c78abad4a1484e9a58bd583d93dfea19
Denis Perevalov <perevalovds@gmail.com>
2017-11-29 17:06:42 +0500

    fix

--- src/ofxOpenVRSyncCoord.cpp
+++ src/ofxOpenVRSyncCoord.cpp
@@ -3,11 +3,24 @@
 //--------------------------------------------------------------
 void ofxOpenVRSyncCoord::setup(string matrix) {
 	reset();
+	vector<string> a = ofSplitString(matrix, " ");
+	if (a.size() >= 16) {
+		vector<float> v(16);
+		for (int i = 0; i < 16; i++) {
+			v[i] = ofToFloat(a[i]);
+		}
+		matrix_.set(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15]);
+	}
 }
 
 //--------------------------------------------------------------
 string ofxOpenVRSyncCoord::packToString() {			//for saving somethere
+	const float *v = matrix_.getPtr();
 	string s = "";
+	for (int i = 0; i < 16; i++) {
+		if (i > 0) s += " ";
+		s += ofToString(v[i]);
+	}
 	return s;
 }
 
@@ -38,6 +51,6 @@ void ofxOpenVRSyncCoord::rotateBy(ofPoint vec, float degrees) { //rotate
 	ofMatrix4x4 m;
 	m.makeRotationMatrix(degrees, vec.x, vec.y, vec.z);
 	matrix_ *= m;
-}
+} 
 
 //--------------------------------------------------------------
